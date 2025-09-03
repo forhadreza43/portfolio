f94eb34277330654a35b4edbd1911bba38fab093
Forhad Reza <forhad.bimt@gmail.com>
2025-09-04 00:12:07 +0600

    fixed theme toggle issue by removing mounted state check

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -8,10 +8,6 @@ import { Button } from "@/components/ui/button";
 
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme();
-  const [mounted, setMounted] = React.useState(false);
-
-  React.useEffect(() => setMounted(true), []);
-
   // console.log("Current theme: ", theme);
   const isDark = (theme ?? "dark") === "dark";
 
@@ -22,12 +18,10 @@ export function ThemeToggle() {
       onClick={() => setTheme(isDark ? "light" : "dark")}
       className="rounded-full"
     >
-      {mounted && (
-        <>
-          <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-        </>
-      )}
+      <>
+        <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      </>
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
