360a89a914374e84014286c6415405a460f706d1
Forhad Reza <forhad.bimt@gmail.com>
2026-01-04 01:38:40 +0600

    Refactor Footer component to remove unnecessary React import

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
-
-const Footer: React.FC = () => (
+const Footer = () => (
    <footer className="text-center py-6 px-4 mt-12 lg:mt-20 border-t border-primary/50 relative">
       <div>
          <p>
