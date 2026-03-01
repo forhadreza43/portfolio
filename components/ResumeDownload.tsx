8c9628c3fe9a236fd1cafe61f2f1d0bd55735f9e
Reza <forhad.bimt@gmail.com>
2026-03-01 23:25:29 +0600

    Update file ID in ResumeDownload component for correct resume download link

--- components/ResumeDownload.tsx
+++ components/ResumeDownload.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { Button } from './ui/button';
 import { Download } from 'lucide-react';
 
+// https://drive.google.com/file/d/1V4wBbcsp0paudReIgUtnBI9QO3Jp65Mu/view?usp=drive_link
+
 const ResumeDownload = () => {
    const handleDownload = () => {
-      const fileId = '1H0CqpI2EBMKySfbUVwWnhrobqWXPW0NT';
+      const fileId = '1V4wBbcsp0paudReIgUtnBI9QO3Jp65Mu';
       const link = document.createElement('a');
       link.href = `https://drive.google.com/uc?export=download&id=${fileId}`;
       link.download = 'Forhad_Reza_Resume.pdf';
