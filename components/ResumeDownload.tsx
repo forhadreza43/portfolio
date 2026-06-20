7a741c8250f61d5ea20bc1b7ae26bbdc9bea86c9
Reza <forhad.bimt@gmail.com>
2026-06-20 10:34:27 +0600

    Update resume download link

--- components/ResumeDownload.tsx
+++ components/ResumeDownload.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
 import { Button } from './ui/button';
 import { Download } from 'lucide-react';
 
 // https://drive.google.com/file/d/1V4wBbcsp0paudReIgUtnBI9QO3Jp65Mu/view?usp=drive_link
+// https://drive.google.com/file/d/1woOkRf7oFyBfh_hd4UDYHnysrZ2LoLyS/view?usp=drive_link
 
 const ResumeDownload = () => {
    const handleDownload = () => {
-      const fileId = '1V4wBbcsp0paudReIgUtnBI9QO3Jp65Mu';
+      const fileId = '1woOkRf7oFyBfh_hd4UDYHnysrZ2LoLyS';
       const link = document.createElement('a');
       link.href = `https://drive.google.com/uc?export=download&id=${fileId}`;
-      link.download = 'Forhad_Reza_Resume.pdf';
+      link.download = 'Resume_of_Forhad_Reza.pdf';
       link.target = '_blank';
       document.body.appendChild(link);
       link.click();
